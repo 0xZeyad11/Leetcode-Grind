c9f3735f0f2c334068cb171acf5557dd4bfdbeba
Zeyad Hesham <48362347+zedoh@users.noreply.github.com>
2024-11-03 19:05:57 +0200

    Time: 3 ms (28.88%), Space: 9.2 MB (41.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true ; 
+        string word =  process(s) ; 
+        int l = 0 ; 
+        int r  = (int)word.size()-1   ; 
+        if(word.size() == 1) return true ; 
+        while(l < r){
+            if(word[l] != word[r]) return false;
+            else{
+                l++ ; 
+                r-- ; 
+            }
+        }
+        return true;
+    }
+private: 
+    string process(string &s){
+        string processed {}; 
+        for(int i = 0  ; i < (int)s.size() ; ++i){
+            if(!isalnum(s[i])) continue ; 
+            else{
+                processed += tolower(s[i]) ; 
+            }
+        }
+
+        return processed ; 
+    }
+};
