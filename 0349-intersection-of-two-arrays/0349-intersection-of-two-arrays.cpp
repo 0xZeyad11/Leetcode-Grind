00d971b53ad0d2f55ebc0ab75079fe527177f9ec
xeenoh <48362347+xeenoh@users.noreply.github.com>
2025-02-14 19:50:04 +0200

    0349-INTERSECTION-OF-TWO-ARRAYS Stats: Time: 0 ms (100%), Space: 13.9 MB (80.93%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res ; 
-        unordered_map<int,int> intersections ; 
-        set<int>st1 (nums1.begin() , nums1.end()) ; 
-        set<int>st2 (nums2.begin() , nums2.end()) ; 
+        vector<int> res;
+        // unordered_map<int,int> intersections ;
+        // set<int>st1 (nums1.begin() , nums1.end()) ;
+        // set<int>st2 (nums2.begin() , nums2.end()) ;
 
-        for(auto i : st1){
-            intersections[i]++ ; 
-        }
-        for(auto i : st2){
-            intersections[i]++ ; 
-        }
+        // for(auto i : st1){
+        //     intersections[i]++ ;
+        // }
+        // for(auto i : st2){
+        //     intersections[i]++ ;
+        // }
 
-        for(auto i:intersections){
-            if(i.second >=2)res.push_back(i.first) ; 
-        }
+        // for(auto i:intersections){
+        //     if(i.second >=2)res.push_back(i.first) ;
+        // }
 
-        return res ;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] == nums2[j]) {
+                if (res.empty() || res.back() != nums1[i]) {
+                    res.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j])
+                i++;
+            else
+                ++j;
+        }
+        return res;
     }
 };
