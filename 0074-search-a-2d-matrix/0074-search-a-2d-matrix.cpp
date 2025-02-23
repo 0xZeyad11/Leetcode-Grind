0b2ea755eab0692bb93113b36ca7c5fdd5dd4cba
xeenoh <48362347+xeenoh@users.noreply.github.com>
2025-02-23 15:10:42 +0200

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 13.3 MB (43.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool res {false} ; 
-        
-        for(int i = 0 ; i < matrix.size() ; ++i){
-            
-            res = BinarySearch(matrix[i], target) ;  
-            if(res) return true;            
+        bool res  = false ; 
+       int m = matrix.size() ;
+       int up = 0 ; 
+       int down = m-1 ; 
+
+       while(up <= down){
+        int Vmid = (up + down)/2  ; 
+        if(matrix[Vmid][0] > target){
+            down = Vmid - 1; 
+        }else if(matrix[Vmid][0] < target){
+            cout<<"Found in row no "<<Vmid<<'\n' ; 
+            res  = binarySearch(matrix[Vmid] , target) ; 
+            if(res) return true ; 
+            else up = Vmid + 1 ; 
         }
-        return false; 
+       } 
+       return res ; 
     }
-    bool BinarySearch(vector<int>& arr, int target){
-        int n = arr.size() ; 
-        int low  = 0 ; 
-        int high = n-1 ; 
-        while(low <= high){
-            int mid = low + (high-low)/2 ; 
-            if(arr[mid] == target) return true ; 
-            else if(arr[mid] < target) low = mid + 1; 
-            else high = mid - 1 ; 
+
+    bool binarySearch(const vector<int>& nums , int target){
+        int n = nums.size() ; 
+        int right = n-1 ;
+        int left = 0 ; 
+        while(left <= right){
+            int mid = left + (right - left)/2 ; // No overflows
+            if(nums[mid] > target) right = mid - 1 ; 
+            else if(nums[mid] < target) left = mid + 1 ; 
+            else return true ; 
         }
-        return false ; 
+
+        return false ;
     }
 };
