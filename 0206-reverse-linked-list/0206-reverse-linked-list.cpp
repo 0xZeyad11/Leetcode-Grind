36c985fd87f8c78f025aaeb66bdc254f887036d5
xeenoh <48362347+xeenoh@users.noreply.github.com>
2025-07-31 20:28:59 +0300

    Time: 3 ms (1.94%), Space: 13.6 MB (19.2%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,14 +11,43 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
+        if(!head) return {}; 
+        vector<ListNode*> nd = BruteForce(head) ; 
+        ListNode* res = connectNodes(nd) ; 
+        return res;
+    }
+
+
+    vector<ListNode*> BruteForce(ListNode* node){
+        vector<ListNode*> nd ; 
+        while(node){
+            nd.push_back(node) ; 
+            node = node->next ;
         }
-        return prev;
+
+        int n = nd.size() ; 
+        for(int i = 0 ; i < n/2 ; ++i){
+            std::swap(nd[i] , nd[n-i-1]) ; 
+        }
+        return  nd ; 
+    }
+
+    void print_debug(vector<ListNode*> &nd){
+        for(auto &i : nd){
+            std::cout<<i->val<<'\t' ; 
+        }
+        std::cout<<'\n';
+    } 
+
+    ListNode* connectNodes(vector<ListNode*> &nd){
+        for (int i  = 0 ; i < nd.size()  ; ++i){
+            if(i == nd.size() - 1){
+                nd[i]->next = nullptr; 
+                break ; 
+            }
+            nd[i]->next = nd[i+1] ; 
+        }
+
+        return nd[0] ; 
     }
 };
