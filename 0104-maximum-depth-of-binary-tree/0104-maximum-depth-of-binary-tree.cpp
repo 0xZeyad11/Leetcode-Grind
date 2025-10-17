2399d4fc826c2fac9890c7afb7d1b4484213f37b
Zeek <48362347+0xZeyad11@users.noreply.github.com>
2025-10-17 17:30:35 +0300

    Time: 3 ms (2.89%), Space: 19 MB (45.16%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -6,25 +6,16 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root)
-            return 0;
-        int res = rec(root);
-        return res;
+       return maxHeight(root) ;  
     }
-
-    int rec(TreeNode* node) {
-       if(!node) return 0;
-       int left = rec(node->left);
-       int right = rec(node->right);
-
-       return 1 + max(right,left);
+    int maxHeight(TreeNode* node){
+        if(!node) return 0 ; 
+        return 1+max(maxHeight(node->left) , maxHeight(node->right)) ;
     }
-
 };
